2fb67e63f5da1b0ada477d409b2ea2d9131c5a4a
lucadidomenico <luca@thirdblock.io>
2024-02-23 11:53:35 +0100

    update api endpoint

--- src/app/api/ethprice/route.ts
+++ src/app/api/ethprice/route.ts
@@ -1,7 +1,7 @@
 export const dynamic = "force-dynamic"; // defaults to auto
 export async function GET() {
 	const res = await fetch(
-		"https://xac37xwsbd.execute-api.us-east-1.amazonaws.com/prod/getethprice"
+		"https://7f9l0a6cmj.execute-api.us-east-1.amazonaws.com/prod/getethprice"
 	);
 	const price: Price = await res.json();
 	return Response.json({ ...price });
